e7952fd58bbc450382b7bcc9cafe99f0319557a6
markus leonard <markusmaiageger@gmail.com>
2021-10-22 21:24:22 +0700

    Remove markus

--- markus/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MarkusConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'markus'
--- markus/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from markus.views import index
-
-urlpatterns = [
-    path('', index, name='uji coba'),
-]
