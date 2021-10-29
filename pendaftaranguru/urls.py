937873c2340485047dfb299d90336c622e2f6216
Adella Rakha Amadea <adella.rakha@ui.ac.id>
2021-10-29 17:15:20 +0700

    push

--- pendaftaranguru/urls.py
+++ pendaftaranguru/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Register_guru, name='pendaftaran guru'),
+    path('', views.Register_guru, name='pendaftaranguru'),
     path('home/', views.home, name='home'),
 ]
