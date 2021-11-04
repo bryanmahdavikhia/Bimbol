a527b000cb474c1d4eae571a093ad1db9b980b91
mutiara.azzahra01 <mutiara.azzahra01@ui.ac.id>
2021-11-04 17:06:39 +0700

    Update testimoni

--- testimoni/forms.py
+++ testimoni/forms.py
@@ -1,2 +1,25 @@
 from django import forms
-from pendaftaransiswa.models import SiswaUser
+from django.forms import fields
+from .models import Testimoni
+
+class TestimoniForm (forms.ModelForm):
+
+    nama = forms.CharField(
+        label='Nama Lengkap', 
+        widget=forms.TextInput(attrs={'placeholder': 'Isi dengan nama lengkap', 'class':'form-control'})
+    )
+
+    kelas = forms.ChoiceField(
+        label='Kelas', 
+		choices=Testimoni.PILIHAN_KELAS,
+        widget=forms.Select(attrs={ 'class':'form-control'})
+    )
+
+    testimoni = forms.CharField(
+        label='Testimoni', 
+        widget=forms.Textarea(attrs={'placeholder': 'Ceritakan pengalaman anda disini', 'class':'form-control'})
+    )
+
+    class Meta:
+        model = Testimoni
+        fields = '__all__'
