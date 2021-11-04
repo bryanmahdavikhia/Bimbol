f9f27122b01338456d4e40fa03289810d9e2819a
halifmuhammad <muhammad.halif@ui.ac.id>
2021-11-04 15:08:40 +0700

    Update Jadwal

--- jadwal/urls.py
+++ jadwal/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, jadwal_json, add_jadwal
+from .views import jadwal_json, jadwal
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
     path('json', jadwal_json, name='json'),
-    path('add', add_jadwal, name='add_jadwal'),
+    path('', jadwal, name='add_jadwal'),
     # path('note-list', note_list, name='note_list')
 ]
--- jadwal/views.py
+++ jadwal/views.py
@@ -7,17 +7,18 @@ from django.http import HttpResponseRedirect
 from django.http.response import HttpResponse
 from django.core import serializers
 
-def index(request):
-    jadwal = Jadwal.objects.all()  # TODO Implement this
-    response = {'jadwal': jadwal}
-    return render(request, 'jadwal.html', response)
+# def index(request):
+#     jadwal = Jadwal.objects.all()  # TODO Implement this
+#     response = {'jadwal': jadwal}
+#     return render(request, 'jadwal.html', response)
 
 def jadwal_json(request):
     data = serializers.serialize('json', Jadwal.objects.all())
     return HttpResponse(data, content_type="application/json")
 
-def add_jadwal(request):
+def jadwal(request):
     failed = False
+    jadwal = Jadwal.objects.all()
     if request.method == 'POST':
         form = JadwalForm(request.POST)
         if form.is_valid():
@@ -27,7 +28,7 @@ def add_jadwal(request):
             failed = True
     
     form = JadwalForm()
-    return render(request, 'jadwal_form.html', {'form':form, 'failed':failed})
+    return render(request, 'jadwal.html', {'form':form, 'failed':failed, 'jadwal': jadwal})
 
 # def note_list(request):
 #     notes = Note.objects.all()  # TODO Implement this
