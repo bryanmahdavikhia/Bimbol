c234d119fb95fcd23bed070728d8c1311baef0d7
Mohammad Bryan Mahdavikhia <mohammad.bryan@ui.ac.id>
2021-11-05 22:57:53 +0700

    bryan belom bener

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import response, JsonResponse
-from userauth.models import CustomUser
+# from userauth.models import CustomUser
 from django.contrib.auth.models import Group
 from django.contrib import messages
 from .forms import BookForm
@@ -13,11 +13,14 @@ def daftar_guru(request):
     return render(request, 'daftar_guru.html', response)
 
 def booking_guru(request):
+    group = Group.objects.get(name='Guru')
+    guru = group.user_set.all()
+    print(guru)
     booking_guru = BookForm()
     if request.is_ajax():
         booking_guru = BookForm(request.POST)
         if booking_guru.is_valid() and request.method == 'POST':
             booking_guru.save()
             return JsonResponse({'msg':'Success'})
-    response = {'booking_guru':booking_guru}
+    response = {'booking_guru':booking_guru, 'guru':guru}
     return render(request, 'pesan.html', response)
