9a18581ea7c426011fab5928d3bb232863cda85a
Ardanisa <ardanisarachma@gmail.com>
2021-11-05 07:11:13 +0700

    tambah js

--- pendaftaransiswa/urls.py
+++ pendaftaransiswa/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
+from .views import UsernameValidationView, EmailValidationView
 
 urlpatterns = [
     path('', views.register_siswa, name='register_siswa'),
     path('home/', views.home, name='homesiswa'),
+    path('validate-username/', csrf_exempt(UsernameValidationView.as_view()),name="validate-username"),
+    path('validate-email/', csrf_exempt(EmailValidationView.as_view()),name="validate-email"),
 ]
--- pendaftaransiswa/views.py
+++ pendaftaransiswa/views.py
@@ -3,6 +3,33 @@ from django.shortcuts import redirect, render
 from django.contrib import messages
 from . forms import RegisterFormSiswa
 from django.contrib.auth.models import Group
+import json
+from django.http import JsonResponse
+from django.views import View
+from userauth.models import CustomUser
+from validate_email import validate_email
+
+
+class EmailValidationView(View):
+	def post(self, request):
+		data = json.loads(request.body)
+		email = data['email']
+		if not validate_email(email):
+			return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+		if CustomUser.objects.filter(email=email).exists():
+			return JsonResponse({'email_error': 'sorry email in use, choose another one'}, status=409)
+		return JsonResponse({'email_valid': True})
+
+class UsernameValidationView(View):
+	def post(self, request):
+		data = json.loads(request.body)
+		username = data['username']
+		if not str(username).isalnum():
+			return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
+		if CustomUser.objects.filter(username=username).exists():
+			return JsonResponse({'username_error': 'sorry username in use, choose another one'}, status=409)
+		return JsonResponse({'username_valid': True})
+
 
 
 def register_siswa(request):
