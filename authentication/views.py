1c35f6170b197f4964c66a72d4d67bc0be230423
Ardanisa <ardanisarachma@gmail.com>
2021-12-31 21:21:36 +0700

    tambah group regis siswa flutter

--- authentication/views.py
+++ authentication/views.py
@@ -3,17 +3,19 @@ from django.contrib.auth import authenticate, login
 from django.http import JsonResponse, response
 from django.views.decorators.csrf import csrf_exempt
 from userauth.models import CustomUser
+from django.contrib.auth.models import Group
 
 from pendaftaransiswa.forms import RegisterFormSiswa
 
 @csrf_exempt
 def signup_siswa(request) :
-    print(request.method)
-    print(request.body)
     form = RegisterFormSiswa(json.loads(request.body))
     if form.is_valid():
-        print("kesini ga")
         user = form.save()
+
+        username = form.cleaned_data.get('username').lower()
+        raw_password = form.cleaned_data.get('password1')
+        account = authenticate(username=username, password=raw_password)
 
         group = Group.objects.get(name='Siswa')
         account.groups.add(group)
@@ -25,4 +27,6 @@ def signup_siswa(request) :
         }, status=200)
     else:
         return JsonResponse(form.errors, status = 400)
+
+
 
